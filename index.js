87b45e1b80dfd4f5026836372ee678bb347d20aa
jencekade <gaephuoc@gmail.com>
2017-12-13 10:18:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  let newObject = ({}, object, {[key]:value});
+  return newObject;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[value] = key;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  let newObject = object;
+  delete object[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
